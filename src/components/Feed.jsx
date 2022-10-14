f4e0dc6d23f6f189b691514cc718c5e8b44c4fb8
freetimezp <kraftukrnet@ukr.net>
2022-10-14 17:17:20 +0300

    05. js-social-04-youtube. Create fetch with axios and RapidAPI. Set category active by click.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Stack, Typography} from "@mui/material";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+
+import {fetchFromAPI} from "../utils/fetchFromAPI";
 
 const Feed = () => {
+    const [selectedCategory, setSelectedCategory] = useState('New');
+
+    useEffect(() => {
+        fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`);
+    }, [selectedCategory])
+
     return (
         <Stack
             sx={{
@@ -22,7 +31,10 @@ const Feed = () => {
                     }
                 }}
             >
-                <Sidebar />
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
 
                 <Typography
                     className="copyright" variant="body2"
@@ -32,6 +44,26 @@ const Feed = () => {
                 >
                     Copyright 2022 Freetime Media
                 </Typography>
+            </Box>
+
+            <Box
+                p={2}
+                sx={{
+                    overflow: 'auto', height: '90vh', flex: 2
+                }}
+            >
+                <Typography
+                    variant="h4"
+                    fontWeight="bold" mb={2}
+                    sx={{
+                        color: 'white',
+                    }}
+                >
+                    {selectedCategory}
+                    <span style={{color: '#F31503'}}> videos</span>
+                </Typography>
+
+                <Videos videos={[]} />
             </Box>
         </Stack>
     );
