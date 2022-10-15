deac007b5a6ed37b2d65ba12e8a3a2d66378a1b9
freetimezp <kraftukrnet@ukr.net>
2022-10-15 17:50:41 +0300

    11. js-social-04-youtube. VideoDetail component.

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,10 +3,12 @@ import {Stack, Box} from '@mui/material';
 import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
 
-const Videos = ({videos}) => {
+const Videos = ({ videos, direction }) => {
+    if(!videos?.length) return "Loading...";
     //console.log(videos);
+    
     return (
-        <Stack direction="row" flexWrap="wrap" justifyContent="center" gap={2}>
+        <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="center" gap={2}>
             {videos?.map((item, idx) => (
                 <Box key={idx}>
                     {item.id.videoId && <VideoCard video={item} />}
